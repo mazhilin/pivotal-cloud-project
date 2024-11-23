47466a45d6bc95c602a4f698db865dbe93bdca70
marklin <2273990296@qq.com>
2024-11-24 00:59:26 +0800

    init pivotal-cloud-boot project

---
+++ pivotal-cloud-parent/pivotal-cloud-boot/src/main/java/com/pivotal/cloud/boot/Aspect.java
@@ -0,0 +1,14 @@
+package com.pivotal.cloud.boot;
+
+/**
+ * @className: com.pivotal.cloud.boot.Aspect
+ * @projectName: PivotalCloud项目
+ * @module: PivotalCloud项目-Aspect类，主要位于应用切面抽象父类接口模块
+ * @content: Aspect-应用切面抽象父类接口
+ * @author: Powered by Marklin
+ * @datetime: 2024-11-24 00:56
+ * @version: 1.0.0
+ * @copyright: Copyright © 2018-2024 PivotalCloud Systems Incorporated. All rights reserved.
+ */
+public interface Aspect {
+}
---
+++ pivotal-cloud-parent/pivotal-cloud-boot/src/main/java/com/pivotal/cloud/boot/Proxy.java
@@ -0,0 +1,14 @@
+package com.pivotal.cloud.boot;
+
+/**
+ * @className: com.pivotal.cloud.boot.Proxy
+ * @projectName: PivotalCloud项目
+ * @module: PivotalCloud项目-Proxy类，主要位于应用代理抽象接口类模块
+ * @content: Proxy-应用代理抽象接口类
+ * @author: Powered by Marklin
+ * @datetime: 2024-11-24 00:54
+ * @version: 1.0.0
+ * @copyright: Copyright © 2018-2024 PivotalCloud Systems Incorporated. All rights reserved.
+ */
+public interface Proxy {
+}
