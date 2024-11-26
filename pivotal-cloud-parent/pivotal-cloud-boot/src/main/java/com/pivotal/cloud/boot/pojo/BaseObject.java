c173aa38a4282fc5d0452930f0f5a4f9aa1aba85
marklin <2273990296@qq.com>
2024-11-26 23:07:37 +0800

    init coverage project code

---
+++ pivotal-cloud-parent/pivotal-cloud-boot/src/main/java/com/pivotal/cloud/boot/pojo/BaseObject.java
@@ -0,0 +1,14 @@
+package com.pivotal.cloud.boot.pojo;
+
+/**
+ * @className: com.pivotal.cloud.boot.pojo.BaseObject
+ * @projectName: PivotalCloud项目
+ * @module: PivotalCloud项目-BaseObject类，主要位于统一基础对象抽象类模块
+ * @content: BaseObject-统一基础对象抽象类
+ * @author: Powered by Marklin
+ * @datetime: 2024-11-26 22:38
+ * @version: 1.0.0
+ * @copyright: Copyright © 2018-2024 PivotalCloud Systems Incorporated. All rights reserved.
+ */
+public abstract class BaseObject {
+}
