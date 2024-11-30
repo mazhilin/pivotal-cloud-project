b8d928cc07b689f35c6d1173fe7f0bcbb0e1d319
marklin <2273990296@qq.com>
2024-11-30 15:06:50 +0800

    init project code

---
+++ pivotal-cloud-parent/pivotal-cloud-starter/pivotal-cloud-starter-boot/src/main/java/com/pivotal/cloud/starter/boot/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * @className: com.pivotal.cloud.starter.boot.package-info
+ * @projectName: 项目
+ * @module: 项目-package-info类，主要位于模块
+ * @content:
+ * @author: Powered by Marklin
+ * @datetime: 2024-11-30 15:01
+ * @version:
+ * @copyright: Copyright © 2018-2024  Systems Incorporated. All rights reserved.
+ */
+package com.pivotal.cloud.starter.boot;
